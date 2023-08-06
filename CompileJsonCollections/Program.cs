77f0493442462eafd603b071710b570154360f75
Marvin Klein <support@marvinkleinmusic.de>
2023-08-06 19:49:11 +0200

    Remove buffId merge

--- CompileJsonCollections/Program.cs
+++ CompileJsonCollections/Program.cs
@@ -8,7 +8,6 @@ namespace CompileJsonCollections
     {
         static async Task Main(string[] args)
         {
-            //await MergeBuffIds();
             string directory_to_copy = args.Length > 0 ? args[0] : string.Empty;
             //// Load all stickers
             await CompileJsonAsync<Sticker>("data/stickers", $"{directory_to_copy}stickers.json");
@@ -48,65 +47,6 @@ namespace CompileJsonCollections
             Console.WriteLine($"Generate file {compiledFileName}...");
             await File.WriteAllTextAsync(compiledFileName, json_items);
             Console.WriteLine($"{compiledFileName} has been compiled successfully!");
-        }
-
-
-        private static async Task MergeBuffIds()
-        {
-
-            string json = await File.ReadAllTextAsync(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data/buff/buff_sticker_data.json"));
-
-            List<BuffDetails> buffDetails = JsonSerializer.Deserialize<List<BuffDetails>>(json)!;
-
-            var buff_list_sorted = buffDetails.OrderBy(x => x.Id).ToArray();
-            json = JsonSerializer.Serialize(buff_list_sorted, new JsonSerializerOptions
-            {
-                WriteIndented = true,
-            });
-
-            await File.WriteAllTextAsync("buff_sticker_data.json", json);
-
-            var files = Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data/stickers"));
-
-            int notFoundAmount = 0;
-            foreach (var file in files)
-            {
-
-                json = await File.ReadAllTextAsync(file);
-                var stickers = JsonSerializer.Deserialize<List<Sticker>>(json)!;
-
-                foreach (var sticker in stickers)
-                {
-                    // Search buff details
-                    var details = buffDetails.FirstOrDefault(x => x.Name.Contains(sticker.name));
-
-                    if (details is not null)
-                    {
-                        sticker.BuffGoodsId = details.Id;
-                        sticker.BuffStickerId = details.SearchId;
-                    }
-                    else
-                    {
-                        sticker.BuffGoodsId = null;
-                        sticker.BuffStickerId = null;
-                        Console.WriteLine($"No data found. Sticker: {sticker.name}");
-                        notFoundAmount++;
-                    }
-                }
-
-                var sticker_list_sorted = stickers.OrderBy(x => x.gen_id).ToArray();
-
-
-
-                json = JsonSerializer.Serialize(sticker_list_sorted, new JsonSerializerOptions
-                {
-                    WriteIndented = true,
-                });
-
-                await File.WriteAllTextAsync(Path.GetFileName(file), json);
-
-            }
-            Console.WriteLine($"Not found: {notFoundAmount}");
         }
     }
 }
