077bb8a5c5182dc59e550f5015babd0a981e4ff7
Marvin Klein <support@marvinkleinmusic.de>
2024-03-26 16:38:50 +0100

    Added more marketplaces

--- CSGencodes/Pages/Index.razor.cs
+++ CSGencodes/Pages/Index.razor.cs
@@ -97,90 +97,11 @@ namespace CSGencodes.Pages
             }
         }
 
-        /// <summary>
-        /// Generates a Steam Community Market URL which searchs for all skins with the selected stickers.
-        /// </summary>
-        /// <returns></returns>
-        private string GetSteamMarketUrl()
-        {
-            if (SelectedStickers.Count == 0)
-            {
-                return string.Empty;
-            }
+  
 
-            StringBuilder sb = new();
-            sb.Append("https://steamcommunity.com/market/search?q=");
+        
 
-            string query = HttpUtility.UrlEncode($"\"{string.Join(",", SelectedStickers.Select(x => x.name))}\"");
-
-            sb.Append(query);
-            sb.Append("&descriptions=1&category_730_ItemSet%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Quality%5B%5D=#p1_price_asc");
-
-            string url = sb.ToString();
-
-            return url;
-        }
-
-        private string GetSkinportUrl()
-        {
-            if (SelectedStickers.Count == 0)
-            {
-                return string.Empty;
-            }
-
-            StringBuilder sb = new();
-            sb.Append("https://skinport.com/market?sticker=");
-
-            string query = $"{string.Join("%2C", SelectedStickers.Select(x => x.name.Replace(" ", "+")))}";
-
-            sb.Append(query);
-
-            sb.Append("&r=erdbeerchen02");
-
-
-            string url = sb.ToString();
-
-            return url;
-        }
-
-        private string GetCsgofloatUrl()
-        {
-            // Example: https://csgofloat.com/db?defIndex=7&paintIndex=282&min=0.1&max=0.7&stickers=%5B%7B%22i%22:%225015%22%7D,%7B%22i%22:%225015%22%7D,%7B%22i%22:%225015%22%7D,%7B%22i%22:%225015%22%7D%5D
-            StringBuilder sb = new();
-            sb.Append("https://csgofloat.com/db?");
-
-            List<string> parameters = new();
-
-            if (SelectedWeapon is not null)
-            {
-                parameters.Add($"defIndex={SelectedWeapon.weapon_id}");
-                parameters.Add($"paintIndex={SelectedWeapon.gen_id}");
-            }
-
-            if (SelectedStickers.Count != 0)
-            {
-                // Example: [{"i":"5015"},{"i":"5015"},{"i":"5015"},{"i":"5015"}]
-                var stickers = SelectedStickers.Take(4);
-                List<string> sticker_values = new();
-                foreach (var sticker in stickers)
-                {
-                    sticker_values.Add($"{{\"i\":\"{sticker.gen_id}\"}}");
-                }
-
-
-                parameters.Add($"stickers=[{string.Join(",", sticker_values)}]");
-            }
-
-
-            if (parameters.Count == 0)
-            {
-                return string.Empty;
-            }
-
-            sb.Append(string.Join("&", parameters));
-
-            return sb.ToString();
-        }
+        
 
         private void RemoveLastSticker()
         {
@@ -193,28 +114,7 @@ namespace CSGencodes.Pages
             SelectedStickers.Sort((x, y) => x.PosId.CompareTo(y.PosId));
         }
 
-        private string? GetBuff163Url()
-        {
-            // Example: https://buff.163.com/market/csgo#tab=selling&page_num=1&extra_tag_ids=16226,16226,16226,16226  
-            List<int> searchIds = [];
-            if (SelectedStickers.Count != 0)
-            {
-                foreach (var sticker in SelectedStickers)
-                {
-                    if (sticker.BuffStickerId is not null)
-                    {
-                        searchIds.Add((int)sticker.BuffStickerId);
-                    }
-                }
-            }
 
-            if (searchIds.Count == 0)
-            {
-                return null;
-            }
-
-            return $"https://buff.163.com/market/csgo#tab=selling&page_num=1&extra_tag_ids={String.Join(",", searchIds)}";
-        }
 
         private async Task OnStickerWearingChange(ChangeEventArgs e, AppliedSticker sticker)
         {
@@ -223,8 +123,5 @@ namespace CSGencodes.Pages
             sticker.Scratched = newValue;
         }
 
-        private async Task OnStickerRotationChange(ChangeEventArgs e, AppliedSticker sticker)
-        {
-        }
     }
 }
